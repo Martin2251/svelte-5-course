884bacc20c4d1078c2f50592a7150856504198ac
Martin Lowe <martinlowe673@gmail.com>
2024-12-05 08:26:39 +0000

    binding

--- src/routes/Headers.svelte
+++ src/routes/Headers.svelte
@@ -3,8 +3,14 @@
     let named = $state('Scott')
     let {name}:{name:string} = $props()
 
+    let names = $state('dave')
+
 </script>
 
 <h1>Helo from {name}</h1>
 
 <h2>{name.replaceAll("t", "x")}</h2>
+
+<h3>{names}</h3>
+
+<input type="text" bind:value={names} />
